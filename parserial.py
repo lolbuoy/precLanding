1b8117dce463866e82419847c0b916600fe41d8e
lolbuoy <115215179+lolbuoy@users.noreply.github.com>
2023-09-28 10:31:15 +0530

    Update parserial.py

--- parserial.py
+++ parserial.py
@@ -1,23 +1,13 @@
+serdev = 'COM43' # serial device of JeVois
+  
 import serial
-
+import time
+  
 def read_serial_data():
-    ser = serial.Serial(
-        port='/dev/ttyACM0',
-        baudrate=115200,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=0
-    )
-    #print("Outside loop")
-    #print(ser)
-    try:
-        while True:
-            print("Inside loop")
-            line = ser.readline()
-            #print line
-            words = line.decode("utf-8").rstrip()
-            print(words)
-            return words  # Return the 'words' list
-    except KeyboardInterrupt:
-        ser.close()
+    with serial.Serial(serdev, 115200) as ser:
+        while 1:
+        # Read a whole line and strip any trailing line ending character:
+
+            line = ser.readline().decode("utf-8").rstrip()
+            #print ("received: {}".format(line))
+            return line
