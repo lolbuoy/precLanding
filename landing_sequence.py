e93802d8f0c18d4cd736ba803c428ba539b03e0c
lolbuoy <115215179+lolbuoy@users.noreply.github.com>
2023-09-28 10:34:30 +0530

    Update landing_sequence.py

--- landing_sequence.py
+++ landing_sequence.py
@@ -3,14 +3,16 @@ from pymavlink import mavutil
 import threading
 import time
 import moveBool
-connection_string = 'udp:192.168.0.123:690'  # Change to your connection string
+import movement
+connection_string = 'udp:192.168.208.247:690'  # Change to your connection string
 
 # Desired GPS coordinates (latitude and longitude) for reaching the location
-desired_latitude = float(input("Enter latitude of location: ")) # Replace with your desired latitude
-desired_longitude = float(input("Enter longitude of location: "))  # Replace with your desired longitude
+desired_latitude =-35.3627141# Replace with your desired latitude
+desired_longitude = 149.1651160# Replace with your desired longitude
 #desired altitude to switch to guided mode
 desiredalt=30
-safealt=3
+safealt=6.5
+descentrate = 1.1
 
 # Connect to the autopilot
 the_connection = mavutil.mavlink_connection(connection_string)
@@ -34,14 +36,12 @@ def land():
         elif alt <= desiredalt and alt>safealt :
             print(f"Altitude is below 30 meters ({alt} meters). Switching to GUIDED mode")
             the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,176, 0, 1, 4, 0, 0, 0, 0, 0)
-            repos_complete = moveBool.control_drone(the_connection)
+                        # Send velocity command for descent
+            repos_complete = moveBool.control_drone(the_connection,descentrate,alt,safealt)
         elif alt<=safealt or repos_complete:
             print(f"safe altitude reached. Switching back to LAND mode...")
-            the_connection.mav.command_long_send(
-                the_connection.target_system,
-                the_connection.target_component,
-                mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, 0, 0)
-
+            the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, 0, 0)
 
 while True:
     gps = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
@@ -70,5 +70,3 @@ def GPS():
     gps = None
     gps = the_connection.recv_match(type='GLOBAL_POSITION_INT', blocking = True)
     return gps.lat , gps.lon , gps.hdg
-            
-          
