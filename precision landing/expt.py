050089039ee3e46430599490bb766d07a63191ee
blueberry706 <110536230+blueberry706@users.noreply.github.com>
2023-10-08 22:14:09 +0530

    7/10 working code

---
+++ precision landing/expt.py
@@ -0,0 +1,72 @@
+import random
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+# Generate a unique filename with a number, date, and time
+def generate_unique_filename(folder):
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_number = 1
+    while True:
+        filename = os.path.join(folder, f"output_{current_time}_{file_number}.csv")
+        file_number += 1
+        return filename
+
+def logging(filename,offsetcorrection,rc1,rc2):
+    with open(filename, 'a') as csv:
+        # Create lists to store x, y, z, rc_channel1, and rc_channel2 values
+        offsetcorrection1 = offsetcorrection[0]
+        x_values = offsetcorrection1[0]
+        y_values = offsetcorrection1[1]
+        z_values = offsetcorrection[2]
+        rc1_values = rc1
+        rc2_values = rc2
+
+        # Write all variable values to the file
+        csv.write(f"{x_values},{y_values},{z_values},{rc1_values},{rc2_values} ")
+
+        # Move to the next line for the next iteration
+        csv.write('\n')
+        print(f"Data has been saved to '{filename}'")
+
+'''def graph(folder,filenamecsv):
+# Generate a unique filename based on the current date and time
+    root_name, extension = os.path.splitext(filenamecsv)
+    filename = os.path.join(folder, f'{root_name}.png')    
+    csv_file_path = filenamecsv
+
+# Initialize lists to store data for variables x, y, z, rc1, and rc2
+    x = []
+    y = []
+    z = []
+    rc1 = []
+    rc2 = []    
+
+    # Read the CSV file and extract data
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        header = next(csv_reader)  # Skip the header row if it exists
+        for row in csv_reader:
+            # Split each row by space and extract numeric values
+            values = row[0].split()
+            x.append(float(values[1]))
+            y.append(float(values[3]))
+            z.append(float(values[5]))
+            rc1.append(float(values[7]))
+            rc2.append(float(values[9]))
+
+    plt.plot(x, z, marker='o', linestyle='-', color='b', label='z vs x')
+    plt.plot(y, z, marker='s', linestyle='--', color='g', label='z vs y')
+    plt.plot(rc1, z, marker='^', linestyle='-.', color='r', label='z vs rc1')
+    plt.plot(rc2, z, marker='d', linestyle=':', color='purple', label='z vs rc2')
+
+    plt.xlabel('X, Y, rc1, rc2')
+    plt.ylabel('z-value')
+    plt.title('z-value vs Variables')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(filename)
+
+# Show the plot (optionals
+    print(f'The plot has been saved as "{filenamecsv}"')'''
---
+++ precision landing/test land seq.py
@@ -0,0 +1,66 @@
+from dronekit import connect, VehicleMode
+from pymavlink import mavutil
+#import onichan as auxiliary
+import landing_sequence
+import threading
+import expt
+import os
+connection_string = 'tcp:localhost:5762'  # Change to your connection string
+the_connection = connect(connection_string)
+print("connected")
+current_time = expt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+desktop_folder = os.path.join(os.path.expanduser("~"), "Desktop", "Jevois_Log")
+
+# Create the "log" folder if it doesn't exist
+os.makedirs(desktop_folder, exist_ok=True)
+
+# Generate a unique filename within the "log" folder
+filename = expt.generate_unique_filename(desktop_folder)
+expt.csv.write(f"X,Y,Z,rc1,rc2/n ")
+
+
+global throttle_flag
+throttle_flag = False
+serdev = None # Keep it as None for running in SITL
+disarmed = False
+# Create a flag to track landing completion
+def throttle_Disarmed_Or_Not(vehicle,serdev,filename):
+    throttle_disarmed = False
+
+    @vehicle.on_message('STATUSTEXT')
+    def handle_status_text1(self, name, msg):
+        # print('Waiting for throttle disarm')
+        if msg.text == "Throttle disarmed":
+            print(msg.text)
+            nonlocal throttle_disarmed
+            throttle_disarmed = True
+
+    while not throttle_disarmed:
+        landing_sequence.run_repositioning(the_connection,throttle_disarmed,serdev,filename)
+    print("Returning control") 
+   # auxiliary.resetQuit(the_connection)
+    return throttle_disarmed
+
+
+def land_Complete_Or_Not(vehicle):
+    land_comp = False
+    throttle_disarmed = False
+    @vehicle.on_message('STATUSTEXT')
+    def handle_status_text(self, name, msg):
+        hasread = landing_sequence.offsetData(serdev)
+        if msg.text == "Land descend started" and hasread[1]:
+             nonlocal land_comp 
+             land_comp = True
+             the_connection.mode = "QLAND"
+    while not land_comp:  #come out of the loop only when both land_comp and throttle_disarmed
+        pass
+    return land_comp
+
+# Call the land_Complete_Or_Not function to monitor landing completion
+isLanding = land_Complete_Or_Not(the_connection)
+#disarmed_thread = threading.Thread(target=throttle_Disarmed_Or_Not(the_connection))
+# Check if landing is complete and perform additional actions if needed
+throttle_Disarmed_Or_Not(the_connection,serdev,filename)
+print("Control returned, proceeding to quit")
+#expt.graph(desktop_folder,filename)
+landing_sequence.resetQuit(the_connection)
